7daf099d9fe46520950608ceb946552c5658537d
Harry <132128413+Paramosi@users.noreply.github.com>
2025-11-27 22:42:10 +0900

    Update 11_27.py

--- simulation/11_27.py
+++ simulation/11_27.py
@@ -39,7 +39,7 @@ if __name__ == "__main__":
         
         #while True:
         error=10
-        for error in range(90):
+        for error in list(range(10, 100, 10)):
             # 왼쪽 모터는 530에서 2470으로, 오른쪽 모터는 2470에서 530으로 움직이도록 계산
             left_pulse = int(2500 - (10/9) * error)
             right_pulse = int(500 + (10/9) * error)
@@ -49,7 +49,7 @@ if __name__ == "__main__":
             time.sleep(0.05)
             print(f"오른쪽 모터 펄스: {right_pulse}")
             pi.set_servo_pulsewidth(PARAFOIL_RIGHT_MOTOR_PIN, right_pulse)
-            time.sleep(0.05)
+            time.sleep(0.1)
 
     except KeyboardInterrupt:
         print("\n프로그램을 종료합니다.")
