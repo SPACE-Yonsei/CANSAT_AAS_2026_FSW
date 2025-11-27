5ffae8cd34cf944eae364cbeab074441f30b0f79
Harry <132128413+Paramosi@users.noreply.github.com>
2025-11-27 22:33:58 +0900

    Update 11_27.py

--- simulation/11_27.py
+++ simulation/11_27.py
@@ -41,8 +41,8 @@ if __name__ == "__main__":
             # 정의된 위치들을 하나씩 순F회
             for error in range(90):
                 # 왼쪽 모터는 530에서 2470으로, 오른쪽 모터는 2470에서 530으로 움직이도록 계산
-                left_pulse = int(PARAFOIL_MOTOR_MIN_PULSE + ((PARAFOIL_MOTOR_MAX_PULSE - PARAFOIL_MOTOR_MIN_PULSE) / 89) * error)
-                right_pulse = int(PARAFOIL_MOTOR_MAX_PULSE - ((PARAFOIL_MOTOR_MAX_PULSE - PARAFOIL_MOTOR_MIN_PULSE) / 89) * error)
+                left_pulse = int(2500 - (10/9) * error)
+                right_pulse = int(500 + (10/9) * error)
                 
                 print(f"왼쪽 모터 펄스: {left_pulse}")                
                 pi.set_servo_pulsewidth(PARAFOIL_LEFT_MOTOR_PIN, left_pulse)
