4ee016be7054508ef73ce3698617ea8394cfc32d
Harry <132128413+Paramosi@users.noreply.github.com>
2026-02-01 16:45:01 +0900

    d

--- motor_simulation/test_motor_real.py
+++ motor_simulation/test_motor_real.py
@@ -20,21 +20,33 @@ if not pi.connected:
     print("❌ pigpio 연결 실패")
     exit()
 
-print("LEFT 모터 0(2500) 이동...")
-pi.set_servo_pulsewidth(LEFT_PIN, left_zero)
+# print("LEFT 모터 0(2500) 이동...")
+# pi.set_servo_pulsewidth(LEFT_PIN, left_zero)
+# time.sleep(5)
+
+# print("LEFT 모터 30 이동...")
+# pi.set_servo_pulsewidth(LEFT_PIN, left_neutral)
+# time.sleep(5)
+
+# print("RIGHT 모터 0(500) 이동...")
+# pi.set_servo_pulsewidth(RIGHT_PIN, right_zero) 
+# time.sleep(5)
+
+# print("RIGHT 모터 30 이동...")
+# pi.set_servo_pulsewidth(RIGHT_PIN, right_neutral)
+# time.sleep(5)
+print("right 0")
+pi.set_servo_pulsewidth(RIGHT_PIN, 0)
 time.sleep(5)
 
-print("LEFT 모터 30 이동...")
-pi.set_servo_pulsewidth(LEFT_PIN, left_neutral)
+print("right 1500")
+pi.set_servo_pulsewidth(RIGHT_PIN, 1500)
 time.sleep(5)
 
-print("RIGHT 모터 0(500) 이동...")
-pi.set_servo_pulsewidth(RIGHT_PIN, right_zero) 
+print("right 2500")
+pi.set_servo_pulsewidth(RIGHT_PIN, 2500)
 time.sleep(5)
 
-print("RIGHT 모터 30 이동...")
-pi.set_servo_pulsewidth(RIGHT_PIN, right_neutral)
-time.sleep(5)
 
 # PWM 신호만 끄고 (모터 토크 해제)
 pi.set_servo_pulsewidth(LEFT_PIN, 0)
