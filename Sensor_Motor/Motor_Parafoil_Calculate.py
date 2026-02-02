fc99da2abd50bbab980e688feeaba7c43febfa10
Harry <132128413+Paramosi@users.noreply.github.com>
2026-02-02 16:56:51 +0900

    d

--- Sensor_Motor/Motor_Parafoil_Calculate.py
+++ Sensor_Motor/Motor_Parafoil_Calculate.py
@@ -73,9 +73,7 @@ def calculate_motor_control(yaw: float) -> float:
     global last_error   
     
 
-    if yaw>180:
-        yaw -= 360
-
+    yaw = quick_angle(yaw)
     # GPS 유효 → 방위각 계산
     if is_gps_valid(1, 1) and not (target_lat == 0.0 and target_lon == 0.0):
         target_azimuth = math.degrees(math.atan2(target_lat - 1, target_lon - 1))
