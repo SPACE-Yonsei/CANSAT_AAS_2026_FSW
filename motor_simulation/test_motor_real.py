95159bac3b37c30702d81cb346a281e9264e0c25
Harry <132128413+Paramosi@users.noreply.github.com>
2026-02-04 15:51:17 +0900

    D

--- motor_simulation/test_motor_real.py
+++ motor_simulation/test_motor_real.py
@@ -2,15 +2,15 @@
 import pigpio
 import time
 
-LEFT_PIN = 12
-RIGHT_PIN = 13
+LEFT_PIN = 13
+RIGHT_PIN = 12
 
 pulse_per_degree = 2000/180
 
 left_zero = 2500
 right_zero = 600
-left_neutral = int(left_zero - 30 * pulse_per_degree)
-right_neutral = int(right_zero + 30 * pulse_per_degree)
+left_neutral = int(left_zero - 120 * pulse_per_degree)
+right_neutral = int(right_zero + 120 * pulse_per_degree)
 
 print(f"left_neutral: {left_neutral}μs")
 print(f"right_neutral: {right_neutral}μs")
@@ -21,20 +21,20 @@ if not pi.connected:
     exit()
 
 print("LEFT 모터 0(2500) 이동..")
-pi.set_servo_pulsewidth(RIGHT_PIN, left_zero)
+pi.set_servo_pulsewidth(LEFT_PIN, left_zero)
 time.sleep(10)
 
 print("LEFT 모터 30 이동...")
-pi.set_servo_pulsewidth(RIGHT_PIN, left_neutral)
+pi.set_servo_pulsewidth(LEFT_PIN, left_neutral)
 time.sleep(10)
 
 print("RIGHT 모터 0(500) 이동...")
-pi.set_servo_pulsewidth(LEFT_PIN, right_zero) 
+pi.set_servo_pulsewidth(RIGHT_PIN, right_zero) 
 time.sleep(10)
 
 
 print("RIGHT 모터 30 이동...")
-pi.set_servo_pulsewidth(LEFT_PIN, right_neutral)
+pi.set_servo_pulsewidth(RIGHT_PIN, right_neutral)
 time.sleep(10)
 
 # print("right 1500")
