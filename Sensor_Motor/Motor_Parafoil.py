b47e3b1a2cee1149b885277ff435b9b250bf831f
Harry <132128413+Paramosi@users.noreply.github.com>
2026-02-04 16:10:50 +0900

    s

--- Sensor_Motor/Motor_Parafoil.py
+++ Sensor_Motor/Motor_Parafoil.py
@@ -12,8 +12,9 @@ pulse_per_degree = 2000/180
 #max_angle_scope = 120 # degrees
 #max_pulse_scope = max_angle_scope * pulse_per_degree
 
-left_zero = 2500
-right_zero = 600
+
+right_zero = 2500
+left_zero = 600
 MAX_ANGLE_SCOPE = 120  # degrees
 
 right_neutral = int(right_zero + MAX_ANGLE_SCOPE * pulse_per_degree)
@@ -65,18 +66,18 @@ def rotate_parafoil_motor(pi, error: float):
         right_pulse = right_neutral
     elif error < 0:
         #turn left
-        effective_error = error + THRESHOLD
+        effective_error = abs(error) - THRESHOLD
         pulse_to_rotate_motor = int(effective_error * pulse_per_degree)
 
         left_pulse = left_neutral + pulse_to_rotate_motor
         right_pulse = right_neutral
     else:
         #turn right
-        effective_error = error - THRESHOLD
+        effective_error = abs(error) - THRESHOLD
         pulse_to_rotate_motor = int(effective_error * pulse_per_degree)
 
         left_pulse = left_neutral
-        right_pulse = right_neutral + pulse_to_rotate_motor
+        right_pulse = right_neutral - pulse_to_rotate_motor
 
     # 현재 상태 저장
     current_left_pulse = left_pulse
