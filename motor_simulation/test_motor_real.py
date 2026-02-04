9dd74d14bcc5bce0f6879c5c36b00a0e402d938d
Harry <132128413+Paramosi@users.noreply.github.com>
2026-02-04 22:24:27 +0900

    d

--- motor_simulation/test_motor_real.py
+++ motor_simulation/test_motor_real.py
@@ -10,8 +10,8 @@ pulse_per_degree = 2000/180
 
 left_zero = 2500
 right_zero = 600
-left_neutral = int(left_zero - 120 * pulse_per_degree)
-right_neutral = int(right_zero + 120 * pulse_per_degree)
+left_neutral = int(left_zero + 120 * pulse_per_degree)
+right_neutral = int(right_zero - 120 * pulse_per_degree)
 
 print(f"left_neutral: {left_neutral}μs")
 print(f"right_neutral: {right_neutral}μs")
@@ -23,27 +23,27 @@ if not pi.connected:
 
 print("LEFT 모터 0(2500) 이동..")
 pi.set_servo_pulsewidth(LEFT_PIN, 2500)
-time.sleep(10)
+time.sleep(7)
 
-# print("LEFT 모터 120 이동...")
-# pi.set_servo_pulsewidth(LEFT_PIN, left_neutral)
-# time.sleep(10)
+print("LEFT 모터 120 이동...")
+pi.set_servo_pulsewidth(LEFT_PIN, left_neutral)
+time.sleep(7)
 
 print("LEFT 모터 0(600) 이동..")
 pi.set_servo_pulsewidth(LEFT_PIN, 600)
-time.sleep(10)
+time.sleep(7)
 
 print("RIGHT 모터 (600) 이동...")
 pi.set_servo_pulsewidth(RIGHT_PIN, 600) 
-time.sleep(10)
+time.sleep(7)
 
-# print("RIGHT 모터 120 이동...")
-# pi.set_servo_pulsewidth(RIGHT_PIN, right_neutral)
-# time.sleep(10)
+print("RIGHT 모터 120 이동...")
+pi.set_servo_pulsewidth(RIGHT_PIN, right_neutral)
+time.sleep(7)
 
 print("RIGHT 모터 (2500) 이동...")
 pi.set_servo_pulsewidth(RIGHT_PIN, 2500) 
-time.sleep(10)
+time.sleep(7)
 
 # print("right 1500")
 # pi.set_servo_pulsewidth(RIGHT_PIN, 1500)
