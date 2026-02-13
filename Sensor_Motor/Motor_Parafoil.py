95cb594051eaf795fcfe16bd42ff2e9a954bc5e9
Harry <132128413+Paramosi@users.noreply.github.com>
2026-02-13 21:24:00 +0900

    s

--- Sensor_Motor/Motor_Parafoil.py
+++ Sensor_Motor/Motor_Parafoil.py
@@ -59,7 +59,7 @@ pid_prev_yaw = None
 # PID 게인 (튜닝 필요)
 Kp = 1.0      # 비례 게인
 Ki = 2   # 적분 게인 (느린 정상상태 오차 제거)
-Kd = 0.3   # 미분 게인 (오버슈트 억제)
+Kd = 0.1   # 미분 게인 (오버슈트 억제)
 INTEGRAL_MAX = 50.0  # Anti-windup 한계
 
 def reset_pid():
