1f8943740655bffeda1402c5789a2f0f04d4f745
Harry <132128413+Paramosi@users.noreply.github.com>
2026-03-24 10:26:21 +0900

    style

--- lib/appargs.py
+++ lib/appargs.py
@@ -46,7 +46,6 @@ class BarometerAppArg:
 
     MID_flight_alt : int = 1301101
     MID_flight_ResetMaxAlt : int = 1301102
-
     MID_motor_alt : int = 1301901  # barometer altitude → motor (for altitude-adaptive guidance)
 
 class ImuAppArg:
