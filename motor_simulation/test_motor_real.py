3cddfc06ebb6596f7dfcb142ef3bba6c2f70d664
Harry <132128413+Paramosi@users.noreply.github.com>
2026-03-27 01:48:48 +0900

    sim

--- motor_simulation/test_motor_real.py
+++ motor_simulation/test_motor_real.py
@@ -3,15 +3,15 @@ import pigpio
 import time
 
 
-RIGHT_PIN = 13
-LEFT_PIN = 12
+RIGHT_PIN = 12
+LEFT_PIN = 13
 
 pulse_per_degree = 2000/180
 
 left_zero = 600
 right_zero = 2500
-left_neutral = int(left_zero + 120 * pulse_per_degree)
-right_neutral = int(right_zero - 120 * pulse_per_degree)
+left_neutral = int(left_zero + 60 * pulse_per_degree)
+right_neutral = int(right_zero - 60 * pulse_per_degree)
 
 print(f"left_neutral: {left_neutral}μs")
 print(f"right_neutral: {right_neutral}μs")
@@ -21,29 +21,23 @@ if not pi.connected:
     print("❌ pigpio 연결 실패")
     exit()
 
-print("LEFT 모터 0(2500) 이동..")
-pi.set_servo_pulsewidth(LEFT_PIN, 2500)
-time.sleep(7)
-
-print("LEFT 모터 120 이동...")
-pi.set_servo_pulsewidth(LEFT_PIN, left_neutral)
-time.sleep(7)
-
 print("LEFT 모터 0(600) 이동..")
 pi.set_servo_pulsewidth(LEFT_PIN, 600)
 time.sleep(7)
 
-print("RIGHT 모터 (600) 이동...")
-pi.set_servo_pulsewidth(RIGHT_PIN, 600) 
+print("LEFT 모터 60 이동...")
+pi.set_servo_pulsewidth(LEFT_PIN, left_neutral)
 time.sleep(7)
 
-print("RIGHT 모터 120 이동...")
+
+print("RIGHT 모터 0(2500) 이동...")
+pi.set_servo_pulsewidth(RIGHT_PIN, 2500)
+time.sleep(7)
+
+print("RIGHT 모터 60 이동...")
 pi.set_servo_pulsewidth(RIGHT_PIN, right_neutral)
 time.sleep(7)
 
-print("RIGHT 모터 (2500) 이동...")
-pi.set_servo_pulsewidth(RIGHT_PIN, 2500) 
-time.sleep(7)
 
 # print("right 1500")
 # pi.set_servo_pulsewidth(RIGHT_PIN, 1500)
