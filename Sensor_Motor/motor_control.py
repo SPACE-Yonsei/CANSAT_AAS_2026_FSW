8f30eb236cd4d403e9f57488de9abdbc13e64ed0
Harry <132128413+Paramosi@users.noreply.github.com>
2026-04-30 15:11:02 +0900

    feat(motor) : reinforce twice control sensitivity

--- Sensor_Motor/motor_control.py
+++ Sensor_Motor/motor_control.py
@@ -30,7 +30,7 @@ RIGHT_MIN_PULSE: int = int(RIGHT_ZERO - MAX_ANGLE_SCOPE * PULSE_PER_DEG)  # μs,
 PULSE_MIN: int = 500   # μs
 PULSE_MAX: int = 2500  # μs
 
-K_pulse: float = PULSE_PER_DEG  # μs/(°/s)
+K_pulse: float = PULSE_PER_DEG*2  # μs/(°/s)
 
 
 def init_control(logger: 'MotorLogger'):
